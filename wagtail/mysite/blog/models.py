88f082c254b86d19af5615d0d3fd05329b7004d8
fernando <masilfer82@gmail.com>
2022-11-02 21:22:02 -0500

    cuarta

--- wagtail/mysite/blog/models.py
+++ wagtail/mysite/blog/models.py
@@ -8,14 +8,13 @@ from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.search import index
 
 # Keep the definition of BlogIndexPage, and add:
-
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('intro')
     ]
-    
+
     def get_context(self, request):
         # Update context to include only published posts, ordered by reverse-chron
         context = super().get_context(request)
@@ -39,11 +38,10 @@ class BlogPage(Page):
         FieldPanel('body'),
         InlinePanel('gallery_images', label="Gallery images"),
     ]
-
 class BlogPageGalleryImage(Orderable):
     page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
     image = models.ForeignKey(
-        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+', blank=True, null=True
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+', blank=True
     )
     caption = models.CharField(blank=True, max_length=250)
 
